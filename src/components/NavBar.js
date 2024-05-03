d785b1c11ab00757932cec32eecc1bdc36c1ea99
matgus217 Djärv <matgus217@gmail.com>
2024-05-03 08:49:19 +0000

    Add new folder and file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,35 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.jpg";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
-    return (
-      <Navbar expand="md" fixed="top">
-        <Container>
-          <Navbar.Brand>
-            <img src={logo} alt="logo" height="60" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-              <Nav.Link>
-                <i className="fas fa-home"></i>Home
-              </Nav.Link>
-              <Nav.Link>
-                <i className="fas fa-sign-in-alt"></i>Sign in
-              </Nav.Link>
-              <Nav.Link>
-                <i className="fas fa-user-plus"></i>Sign up
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  };
-  
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand className={styles.NavBar}>
+          <img  src={logo} alt="logo" height="60" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+            <Nav.Link>
+            <i className="fa-solid fa-phone-volume"></i>Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
 export default NavBar;
