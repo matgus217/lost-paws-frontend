65f65f74d1e3c0b5310b4c82225c2ae4e625f2dc
matgus217 Djärv <matgus217@gmail.com>
2024-05-03 07:15:52 +0000

    Add a button in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hej</h1>
+     <Button variant="primary">Primary</Button>
      
     </div>
   );
